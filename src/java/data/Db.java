999bc2bbd554edcec14aa144ca0dd0427fc78cb7
Bearukun <bearukun@gmail.com>
2016-09-08 11:15:20 +0200

    Added data package.

---
+++ src/java/data/Db.java
@@ -0,0 +1,28 @@
+package data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Db {
+
+    private static String driver = "com.mysql.jdbc.Driver";
+    private static String url = "jdbc:mysql://localhost/x070916";
+    private static String user = "testuser";
+    private static String password = "testuser";
+
+    public static Connection getConnection() {
+        Connection conn = null;
+
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            //se hele sekvenst til det gik galt
+            ex.printStackTrace();
+        }
+        return conn;
+    }
+}
